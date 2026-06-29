c7d2ad728a4b25200a56bb8fa160295e149f98e4
DjAbama <kurochkaseva08@gmail.com>
2026-06-29 15:04:36 +0300

    Add macros for unsigned int

--- main.asm
+++ main.asm
@@ -23,6 +23,37 @@ section .data
         syscall
 %endmacro
 
+%macro PrintInteger 1
+
+    mov rax, %1
+    xor rcx, rcx
+    xor rdx, rdx
+    mov rbx, 10
+    
+    %%GetNumber:
+        div rbx
+        add rdx, 48
+        dec rsp
+        mov [rsp], dl
+        add rcx, 1
+        xor rdx, rdx
+
+        cmp rax, 0
+        je %%Print2
+        jmp %%GetNumber
+
+
+    %%Print2:
+        mov rax, 1
+        mov rdi, 1
+        mov rsi, rsp 
+        mov rdx, rcx
+        syscall
+
+        add rsp, rcx
+
+%endmacro
+
 
 section .text
     global _start
@@ -30,6 +61,7 @@ section .text
 _start:
 
     PrintString text
+    PrintUnsignedInteger 321312
 
     mov rax, 60
     mov rdi, 0
